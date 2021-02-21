9fcd488692ac8d2480abf963d1455cb275219823
IngabireTina <ingabire067@gmail.com>
2021-02-21 18:42:34 -0500

    added content in pip, directive and class plus method in quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote';
+import { ThrowStmt } from '@angular/compiler';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [];
+  
+
+  toggleDetails(index) {
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
+  
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.push(quote);
+  }
+
+  deleteQuote(isDelete, index) {
+    if(isDelete){
+      let toDelete = window.confirm(`Are you sure you want to delete this quote?`);
+
+      if(toDelete) {
+       this.quotes.splice(index, 1);
+      }
+    }
+  }
+
+  increaseUpvote(isIncreaseUpvote, index) {
+    if(isIncreaseUpvote){
+      this.quotes[index].numberOfUpvote += 1;
+    }
+  }
+
+  increaseDownvote(isIncreaseDownvote, index) {
+    if(isIncreaseDownvote){
+      this.quotes[index].numberOfDownvote += 1;
+    }
+  }
 
   constructor() { }
 
