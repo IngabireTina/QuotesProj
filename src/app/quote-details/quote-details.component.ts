0d0d93401fc41ad6c6697d4116fb25fbb0312b9c
IngabireTina <ingabire067@gmail.com>
2021-02-21 19:17:42 -0500

    display quotes details

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  @Input() quote: Quote;
+  @Output() isDelete = new EventEmitter<Boolean>();
+  @Output() isIncreaseUpvote = new EventEmitter<any>();
+  @Output() isIncreaseDownvote = new EventEmitter<any>();
+
+  quoteDelete(toDelete:boolean) {
+    this.isDelete.emit(toDelete);
+  }
+
+  increaseUpvote() {
+    this.isIncreaseUpvote.emit(true);
+  }
+
+  increaseDownvote() {
+    this.isIncreaseDownvote.emit(true);
+  }
 
   constructor() { }
 
